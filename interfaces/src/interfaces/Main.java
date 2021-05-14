6f0d2c565b3f9512d11ab01dd3ebfe270e5ccb09
Administrator <Administrator@DESKTOP-49AFC58>
2021-05-14 15:34:21 +0300

    java

---
+++ interfaces/src/interfaces/Main.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Logger[] loggers = {new SmsLogger(), new EmailLogger(),new FileLogger()}; 
+		CustomerManager customerManager = new CustomerManager(loggers);
+		Customer engin = new Customer(1,"þeydanur","kuvvetli");
+		customerManager.add(engin);
+
+	}
+
+}
